ef14c83dda3fa7dfcf02faaf0c317a3795645ec1
khushitomar <khushitomar057@gmail.com>
2026-01-18 16:08:42 +0530

    Add program to find the greatest of two numbers

---
+++ greatest of two.py
@@ -0,0 +1,7 @@
+x= int(input("put value of first number:"))
+y= int(input("put value of second number:"))
+# checking whichone is greater
+if(x>y):
+    print(f"x which is {x}is greater than y that is {y}")
+else:
+    print("y is geater than x")
