a24ba0f716951a91837bc3ab39eb5d1dbad63d7a
khushitomar <khushitomar057@gmail.com>
2026-02-09 11:28:48 +0530

    Add function to convert string to uppercase

---
+++ uppercase.py
@@ -0,0 +1,9 @@
+str1=input("enter any string:")
+result=""
+for ch in str1:
+    if "a"<= ch <="z":
+        result+= chr(ord(ch)-32)
+    else:
+        result+= ch
+            
+print(" given string in uppercase:",result)
