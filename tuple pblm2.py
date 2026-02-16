ee1c0abbf2d7adce40ce6b3b0d5ffed0c11a5d86
khushitomar <khushitomar057@gmail.com>
2026-02-16 11:51:05 +0530

    Add student data extraction from tuples

---
+++ tuple pblm2.py
@@ -0,0 +1,16 @@
+students=[(18,"khushi",27),
+          (18,"vrunda",57),
+          (18,"rutva",26),
+]
+age_list=[]
+name_list=[]
+roll_list=[]
+
+for i in students:
+    age_list.append(i[0])
+    name_list.append(i[1])
+    roll_list.append(i[2])
+
+print(age_list)
+print(name_list)
+print(roll_list)
